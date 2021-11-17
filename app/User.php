e9456a33b399396f0be217b50f1029a1a7c28c0a
Shogo Okagawa <okagawa1981@gmail.com>
2021-11-17 15:04:45 +0900

    Model/Controller create

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Goal.phpとのリレーションを追加
+    public function goals()
+    {
+        return $this->hasMany('App\Goal');
+    }
+
+    public function todos()
+    {
+        return $this->hasMany('App\Todo');
+    }
 }
